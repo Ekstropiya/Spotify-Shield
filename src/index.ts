a493813add1f119751d607857bf30183a1ffc488
Talon Vanderbeken <talon.vanderbeken@protonmail.com>
2022-01-02 01:21:51 -0500

    Optimized code to function in a container.

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,39 @@ import express from 'express';
 import cors from 'cors'
 import { api } from './api/api';
 import { Spotify } from './spotify';
+import * as fs from 'fs';
+
+if (process.env['SS_DOCKER']) {
+    if (!fs.existsSync("/etc/spotifyshield/config")) {
+        fs.copyFileSync("/opt/spotifyshield/.env.example", "/etc/spotifyshield/config");
+    }
+}
 
 loadConfig();
+loadConfig({ path: '/etc/spotifyshield/config' });
 
 const onError = (error: any) => {
     console.error(`An error ocurred in the Spotify client: ${error}`);
 }
 
+const clientId = process.env['CLIENT_ID'];
+const clientSecret = process.env['CLIENT_SECRET'];
+const redirectUri = process.env['REDIRECT_URI'];
+
+if ((!clientId || !clientSecret || !redirectUri) || (clientId == "" || clientSecret == "" || redirectUri == "")) {
+    console.error("Missing configuration properties.");
+    process.exit(1);
+}
+
+const cacheFile = process.env['CACHE_FILE'] || 'data/cache';
+const cache = (process.env['CACHE'] || 'true') == 'true';
+
 const client = new Spotify(
-    process.env['CLIENT_ID']!,
-    process.env['CLIENT_SECRET']!,
-    process.env['REDIRECT_URI']!,
-    process.env['CACHE_FILE']!,
-    process.env['CACHE']! == 'true',
+    clientId,
+    clientSecret,
+    redirectUri,
+    cacheFile,
+    cache,
     onError
 );
 
@@ -25,7 +45,7 @@ app.use('/', api(client));
 app.use(cors());
 
 const addr = process.env['BIND_ADDR'] || '0.0.0.0';
-const port = parseInt(process.env['BIND_PORT'] || '6768');
+const port = parseInt(process.env['BIND_PORT'] || '8080');
 
 app.listen(port, addr, () => {
     console.log(`[+] Server up on ${addr}:${port}.`)
