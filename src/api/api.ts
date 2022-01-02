0d3865849b4e141c7c34a82ac0e8a30f4a279e06
Talon Vanderbeken <talon.vanderbeken@protonmail.com>
2022-01-01 23:24:52 -0500

    De-spaghettied code.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,25 +1,13 @@
 import { Router } from 'express';
-import { config as loadConfig } from 'dotenv';
 import { Spotify } from '../spotify';
 import { auth } from './auth';
 import { spotify } from './spotify'
 
-loadConfig();
+export const api = (client: Spotify): Router => {
+    const api: Router = Router();
+    
+    api.use('/auth', auth(client));
+    api.use('/spotify', spotify(client));
 
-const clientId: string = process.env["CLIENT_ID"]!;
-const clientSecret: string = process.env["CLIENT_SECRET"]!;
-const redirectUri: string = process.env["REDIRECT_URI"]!;
-const cacheFile: string = process.env["CACHE_FILE"]!;
-const cache: boolean = process.env["CACHE"]! == "true";
-
-const api: Router = Router();
-
-const onError = (error: any) => {
-    console.error(`An error ocurred in the Spotify client: ${error}`);
+    return api;
 }
-const client: Spotify = new Spotify(clientId, clientSecret, redirectUri, cacheFile, cache, onError);
-
-api.use('/auth', auth(client));
-api.use('/spotify', spotify(client));
-
-export { api };
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,26 @@ import { config as loadConfig } from 'dotenv';
 import express from 'express';
 import cors from 'cors'
 import { api } from './api/api';
+import { Spotify } from './spotify';
 
 loadConfig();
 
+const onError = (error: any) => {
+    console.error(`An error ocurred in the Spotify client: ${error}`);
+}
+
+const client = new Spotify(
+    process.env["CLIENT_ID"]!,
+    process.env["CLIENT_SECRET"]!,
+    process.env["REDIRECT_URI"]!,
+    process.env["CACHE_FILE"]!,
+    process.env["CACHE"]! == "true",
+    onError
+);
+
 const app = express();
 
-app.use("/", api);
+app.use("/", api(client));
 app.use(cors());
 
 const addr = process.env["BIND_ADDR"] || "0.0.0.0";
